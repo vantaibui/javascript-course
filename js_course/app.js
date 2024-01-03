86d9a58a5841debda7268c936e81717ccb60e2c0
vantaibui <vantaibui92@gmail.com>
2024-01-03 22:41:20 +0700

    fix-conflict

--- js_course/app.js
+++ js_course/app.js
@@ -7,16 +7,10 @@ const teacherLeanring = function() {
 
 teacherLeanring(); // A1
 teacherLeanring(); // A2
-<<<<<<< HEAD
 teacherLeanring(); // A3
 teacherLeanring(); // A4
 
-=======
 teacherLeanring(); // B1
-<<<<<<< HEAD
->>>>>>> be75389 (B1)
-=======
 teacherLeanring(); // B2
->>>>>>> 08f17e6 (B2)
 
 
